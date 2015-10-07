9f1b3d9ce9a46d83dd805fde103608990614bf5f
unknown <dbrown@dbrownhome.northamerica.corp.microsoft.com>
2015-10-06 23:11:23 -0700

    Supporting project for SqlPackageTask

---
+++ WebOps/WhatIsMyIP/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿namespace WhatIsMyIP
+{
+   #region using
+
+   using System.Web.Http;
+
+   #endregion
+
+   public static class WebApiConfig
+   {
+      #region Public Methods and Operators
+
+      public static void Register(HttpConfiguration config)
+      {
+         // Web API configuration and services
+
+         // Web API routes
+         config.MapHttpAttributeRoutes();
+
+         config.Routes.MapHttpRoute(name: "DefaultApi", routeTemplate: "api/{controller}/{id}", defaults: new { id = RouteParameter.Optional });
+      }
+
+      #endregion
+   }
+}
---
+++ WebOps/WhatIsMyIP/Controllers/IPController.cs
@@ -0,0 +1,28 @@
+﻿namespace WhatIsMyIP.Controllers
+{
+   #region using
+
+   using System.Web.Http;
+
+   #endregion
+
+   public class IPController : ApiController
+   {
+      #region Public Methods and Operators
+
+      public string Get()
+      {
+         if (this.Request.Properties.ContainsKey("MS_HttpContext"))
+         {
+            dynamic ctx = this.Request.Properties["MS_HttpContext"];
+            if (ctx != null)
+            {
+               return ctx.Request.UserHostAddress;
+            }
+         }
+         return null;
+      }
+
+      #endregion
+   }
+}
